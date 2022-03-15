78cf7d940c8f038f96782d21e0cc04f41821c0ed
Hossam Ibraheem <hossamibraheem2014@gmail.com>
2022-03-15 10:33:39 +0200

    Edit ALLOWED_HOSTS depend on nginx configuration

--- Traveling/settings.py
+++ Traveling/settings.py
@@ -14,7 +14,8 @@ SECRET_KEY = 'django-insecure-b_bjp44joo*vq^p1@=ltnbkn0^pmxbks##!%invfry3jyp-hs$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+#django is the upstream in nginx conf
+ALLOWED_HOSTS = ['django']
 
 
 # Application definition
@@ -111,6 +112,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
